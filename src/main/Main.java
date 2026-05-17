3051aa95ac10b511851e9098212f6a867ae0508c
ChiuTien <chiumandresychristiant@gmail.com>
2026-05-17 16:31:53 +0300

    Test FIFO ok

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,20 +2,24 @@ package main;
 
 import java.util.List;
 
+import models.LotStock;
 import models.Mouvements_stock;
 import models.Produits;
 import models.StockDetail;
 import repositories.Dao;
+import services.StockService;
 
 public class Main {
     public static void main(String[] args) throws Exception{
         System.out.println("On va faire des testes");
         Dao db = new Dao();
-        StockDetail resultat = new StockDetail();
+        StockService service = new StockService();
         Produits p = db.getById(Produits.class, 1);
         List<Mouvements_stock> mvnt = db.getAll(Mouvements_stock.class);
-        for (Mouvements_stock mouvements_stock : mvnt) {
-            System.out.println(mouvements_stock.getDate_mouvement());
+        StockDetail resultat = service.getStock(p, mvnt, 8);
+        for (LotStock lotStock : resultat.getLotsRestants()) {
+            System.out.println(lotStock.getQuantite()+" => "+lotStock.getPrix_unitaire());
         }
+        System.out.println(resultat.getQuantiteTotale()+" => "+resultat.getValeurTotale());
     }
 }
