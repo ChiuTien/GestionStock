596e1a60956b6eb25680ee2826109bdf219ddfb9
ChiuTien <chiumandresychristiant@gmail.com>
2026-05-18 23:21:46 +0300

    Reglage des warnings

--- src/ui/panels/StockPanel.java
+++ src/ui/panels/StockPanel.java
@@ -11,7 +11,6 @@ import javax.swing.table.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.time.LocalDate;
 import java.util.List;
 
